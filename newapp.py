166ac1b8a1f9d98a3e1aa03f9e2aca1deef7b8a0
Nathan Reyes <nreyes4478@gmail.com>
2026-09-18 16:49:59 -0400

    added collection class, started integrating SQLite

--- newapp.py
+++ newapp.py
@@ -1,7 +1,19 @@
 from flask import Flask, render_template, url_for
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
+
+class Collection(db.Model):
+    id = db.column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_entry = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Book %r>' % self.id
+
+
 @app.route('/')
 def index():
     return render_template('index.html')
